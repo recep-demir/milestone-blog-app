de908b9bc9e72e57401a26e46780e871ff338fea
recep-demir <demir.rp@gmail.com>
2025-02-17 13:40:52 +0100

    .

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Dashboard from "../pages/Dashboard";
 import About from "../pages/About";
+import PrivateRouter from "./PrivateRouter";
+import NewBlog from "../pages/NewBlog";
 
 const AppRouter = () => {
   return (
@@ -11,11 +13,9 @@ const AppRouter = () => {
     <Routes>
         <Route path="/" element={<Dashboard/>}/>
         <Route path="/about" element = {<About/>}/>
-
-
-
-
-
+        <Route path="/auth" element = {<PrivateRouter/>}>
+        <Route path ="" element={<NewBlog/>} />       
+        </Route>
 
 
     </Routes>
