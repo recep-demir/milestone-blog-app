ca4b1e2f7c880fd0ec59d9bde6cddcb8fe674ca3
recep-demir <demir.rp@gmail.com>
2025-02-19 22:22:58 +0100

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import { createTheme, ThemeProvider } from '@mui/material';
-import AppRouter from './router/AppRouter'
-import { Provider } from 'react-redux';
-import store from "./app/store";
-import { ToastContainer } from 'react-toastify';
-
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import AppRouter from "./router/AppRouter";
+import { Provider } from "react-redux";
+import store, { persistor } from "./app/store";
+import { ToastContainer } from "react-toastify";
+// import ErrorBoundary from "./components/ErrorBoundary";
+import { PersistGate } from "redux-persist/integration/react";
 
 
 function App() {
@@ -23,7 +24,9 @@ function App() {
   return (
     <ThemeProvider theme = {theme}>
       <Provider store={store}>
-          <AppRouter />
+      <PersistGate loading={null} persistor={persistor}>
+            <AppRouter />
+          </PersistGate>
         </Provider>
         <ToastContainer/>
 
--- src/app/store.jsx
+++ src/app/store.jsx
@@ -1,11 +1,39 @@
-import { configureStore } from '@reduxjs/toolkit';
-import authReducer from '../features/authSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "../features/authSlice";
+import blogReducer from "../features/blogSlice";
+import { persistStore, persistReducer,REHYDRATE,FLUSH,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
-export const store = configureStore({
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, authReducer);
+
+const store = configureStore({
   reducer: {
-    auth: authReducer,
+    auth: persistedReducer,
+    blog:blogReducer
   },
+ 
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+
+
   devTools: process.env.NODE_ENV !== "production",
 });
+
+export let persistor = persistStore(store) 
+
 
 export default store;
