50a05d73e7769a987a4d142f0c1c09acb3c7c5b4
recep-demir <demir.rp@gmail.com>
2025-02-20 10:10:52 +0100

    .

---
+++ src/hooks/useBlogCalls.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const useBlogCalls = () => {
+
+    const getBlogs = {
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+
+
+
+  return (
+    <div>useBlogCalls</div>
+  )
+}
+
+export default useBlogCalls
