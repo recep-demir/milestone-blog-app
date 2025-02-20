4d41267a8c4b5a860cbccbf893ba66d9f1c43944
recep-demir <demir.rp@gmail.com>
2025-02-20 10:48:50 +0100

    .

--- src/hooks/useBlogCalls.jsx
+++ src/hooks/useBlogCalls.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { fetchFail, fetchStart } from '../features/blogSlice'
 
 const useBlogCalls = () => {
+    const dispatch =useDispatch()
+    const BASE_URL=import.meta.env.VITE_BASE_URL
 
-    const getBlogs = {
+    const getBlogs = async () => {
+        dispatch(fetchStart())
         try {
-            
+            const {data}= await axios(`${BASE_URL}blogs`,
+                {
+                    headers: {
+                    Authorization:`Token ${token}`,
+                    },
+                }
+            )
+            }
         } catch (error) {
+            dispatch(fetchFail())
             
         }
     }
