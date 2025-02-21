fb924abfcc962fc01c739bbac0c01b865c509b6c
recep-demir <demir.rp@gmail.com>
2025-02-21 22:34:53 +0100

    add Pagination

--- src/components/blog/BlogCard.jsx
+++ src/components/blog/BlogCard.jsx
@@ -7,51 +7,83 @@ import Typography from "@mui/material/Typography";
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import CommentIcon from '@mui/icons-material/Comment';
 import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
-import { btnStyle } from "../../styles/globalStyles";
-import { Button } from "@mui/material";
+import { Button, Box } from "@mui/material";
 
 
-export default function BlogCard({ blog, setInitialState, handleOpen }) {
-  const { _id, title, createdAt, content, image } = blog;
+
+export default function BlogCard({ blog, handleDetails }) {
+  const { _id, title, createdAt, content, image,likes } = blog;
 
   
+const formatDate = (dateString) => {
+  const date = new Date(dateString);
+  return `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`;
+};
+
   return (
     <Card
+    id={_id}
       sx={{
         height: 390,
-        display:"flex",
+        display: "flex",
         flexDirection: "column",
         justifyContent: "space-between",
         padding: "0.5rem",
       }}
     >
-      <CardContent>
+      <CardContent sx={{ textAlign: "center" }}>
         <CardMedia
-        sx={{ height: 140, objectFit: "contain" }}
-        image={image}
-        component="img"
-        title={title}
-      />
-      <Typography gutterBottom variant="h5" component="div">
+          sx={{ height: 140, objectFit: "contain", margin: "auto" }}
+          image={image}
+          component="img"
+          title={title}
+        />
+        <Typography mt ={3} gutterBottom variant="h5" component="div">
           {title}
         </Typography>
-        <Typography variant="body2" sx={{ color: "text.secondary" }}>
+      </CardContent>
+
+      <CardContent>
+        <Typography
+          variant="body2"
+          sx={{
+            marginTop: "-1rem",
+            color: "text.secondary",
+            display: "-webkit-box",
+            WebkitBoxOrient: "vertical",
+            WebkitLineClamp: 2,
+            overflow: "hidden",
+          }}
+        >
           {content}
         </Typography>
       </CardContent>
-      
+
       <CardContent>
-        <Typography variant="body2">Published Date: {createdAt}</Typography>
+        <Typography variant="body2">
+          Published Date: {formatDate(createdAt)}
+        </Typography>
       </CardContent>
 
-      <CardActions sx={{ justifyContent: "center", gap: 2 }}>
-        <FavoriteIcon/>
-        <CommentIcon/>
-        <RemoveRedEyeIcon/>
-        <Button variant="contained">Contained</Button>
-
+      <CardActions sx={{ display: "flex", justifyContent: "space-between" }}>
+      <Box sx={{ display: "flex", gap: 3 }}>
+        <Box sx={{ display: "flex", gap: 0.5 }}>
+          <FavoriteIcon />
+          <Typography>{likes.length}</Typography>
+        </Box>
+        <Box sx={{ display: "flex", gap: 0.5 }}>
+          <CommentIcon />
+          <Typography>0</Typography>
+        </Box>
+        <Box sx={{ display: "flex", gap: 0.5 }}>
+          <RemoveRedEyeIcon />
+          <Typography>0</Typography>
+        </Box>
+        </Box>
+        <Button variant="contained" onClick={() => handleDetails(_id)}>
+          Read More
+        </Button>
       </CardActions>
-      
     </Card>
   );
 }
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,28 +1,44 @@
-import React from 'react'
-import { useEffect } from 'react'
-import useBlogCalls from '../hooks/useBlogCalls'
-import { Container, Typography } from '@mui/material'
-import { useSelector } from 'react-redux'
-import BlogCard from '../components/blog/BlogCard'
-
+import React, { useEffect, useState } from "react";
+import useBlogCalls from "../hooks/useBlogCalls";
+import { Container, Grid, Pagination } from "@mui/material";
+import { useSelector } from "react-redux";
+import BlogCard from "../components/blog/BlogCard";
 
 const Dashboard = () => {
-  const {getBlogs}=useBlogCalls()
-  const {blogs} =useSelector(state => state.blog)
+  const { getBlogs } = useBlogCalls();
+  const { blogs } = useSelector((state) => state.blog);
+
+  
+  const [page, setPage] = useState(1);
+  const blogsPerPage = 10;
+  const totalPages = Math.ceil((blogs?.length || 0) / blogsPerPage);
+  const currentBlogs = blogs?.slice(
+    (page - 1) * blogsPerPage, 
+    page * blogsPerPage
+  );
 
   useEffect(() => {
     getBlogs();
-  }, [])
+  }, []);
 
   return (
     <Container>
-      {blogs?.map((blog)=>(
-        <BlogCard key={blog.id} blog={blog}/>
-      ))}
-      
-      
-    </Container>
-  )
-}
+      <Grid container spacing={2} mt={2}>
+        {currentBlogs?.map((blog) => (
+          <Grid item xs={12} md={6} lg={4} xl={3} key={blog._id}>
+            <BlogCard blog={blog} />
+          </Grid>
+        ))}
+      </Grid>
 
-export default Dashboard
+      <Pagination
+        count={totalPages}
+        page={page}
+        onChange={(event, value) => setPage(value)}
+        sx={{ display: "flex", justifyContent: "center", marginTop: 3 }}
+      />
+    </Container>
+  );
+};
+
+export default Dashboard;
