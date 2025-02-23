e5f00a0dce5f3f008e3dc0bbd08542d8bfd5da3f
recep-demir <demir.rp@gmail.com>
2025-02-23 15:42:50 +0100

    .

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -5,17 +5,28 @@ import FavoriteIcon from "@mui/icons-material/Favorite";
 import CommentIcon from "@mui/icons-material/Comment";
 import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye";
 import PersonIcon from "@mui/icons-material/Person";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import useBlogCalls from "../hooks/useBlogCalls";
+import { useEffect } from "react";
 
 const Detail = () => {
   const location = useLocation();
-  const blog = location.state?.blog; // Blog verisini al
+  const blog = location.state?.blog;
+  const dispatch =useDispatch();
+  const {comments} = useSelector(state => state.blog)
+  const {getComments} = useBlogCalls()
+
+  useEffect(()=>{
+    getComments()
+  },[])
+
+  const blogComments = comments.filter((comment) => comment.blogId === blog._id);
+
 
   return (
     <Container maxWidth="md" sx={{ mt: 4 }}>
-      {/* Blog Resmi */}
       <CardMedia component="img" height="300" image={blog.image} alt={blog.title} sx={{ borderRadius: 2 }} />
-
-      {/* Yazar Bilgileri */}
       <Box sx={{ display: "flex", alignItems: "center", mt: 2 }}>
         <Avatar sx={{ bgcolor: "primary.main", mr: 1 }}>
           <PersonIcon />
@@ -28,17 +39,14 @@ const Detail = () => {
         </Typography>
       </Box>
 
-      {/* Blog Başlığı */}
       <Typography variant="h4" sx={{ mt: 2, fontWeight: "bold" }}>
         {blog.title}
       </Typography>
 
-      {/* Blog İçeriği */}
       <Typography variant="body1" sx={{ mt: 2 }}>
         {blog.content}
       </Typography>
 
-      {/* İkonlar */}
       <Box sx={{ display: "flex", gap: 3, mt: 3 }}>
         <Box sx={{ display: "flex", alignItems: "center", gap: 0.5 }}>
           <FavoriteIcon />
@@ -54,10 +62,8 @@ const Detail = () => {
         </Box>
       </Box>
 
-      {/* Yorum Yazma Alanı */}
       <TextField label="Write a comment..." fullWidth multiline rows={3} sx={{ mt: 3 }} />
 
-      {/* Yorum Ekle Butonu */}
       <Button variant="contained" sx={{ mt: 2 }}>
         Add Comment
       </Button>
@@ -65,18 +71,19 @@ const Detail = () => {
       {/* Yorumlar */}
       <Box sx={{ mt: 4 }}>
         <Typography variant="h6">Comments</Typography>
-        <Box sx={{ mt: 2, p: 2, border: "1px solid #ddd", borderRadius: 2 }}>
-        
-          <Typography variant="body2" sx={{ fontWeight: "bold" }}>
-            Staff
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Sat, 01 Feb 2025
-          </Typography>
-          <Typography variant="body1" sx={{ mt: 1 }}>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique labore repellendus quibusdam consequuntur quae.
-          </Typography>
-        </Box>
+        {blogComments.map((comment) => (
+          <Box key={comment._id} sx={{ mt: 2, p: 2, border: "1px solid #ddd", borderRadius: 2 }}>
+            <Typography variant="body2" sx={{ fontWeight: "bold" }}>
+              {comment.userId?.username || "Anonymous"}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {new Date(comment.createdAt).toLocaleString("en-GB", { weekday: "short", day: "2-digit", month: "short", year: "numeric" })}
+            </Typography>
+            <Typography variant="body1" sx={{ mt: 1 }}>
+              {comment.content}
+            </Typography>
+          </Box>
+        ))}
       </Box>
     </Container>
   );
