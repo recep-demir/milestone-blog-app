5d3a4368e498306e4fa02d9af1bd67162c43c584
recep-demir <demir.rp@gmail.com>
2025-02-24 23:27:50 +0100

    edit dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -25,7 +25,7 @@ const Dashboard = () => {
     <Container>
       <Grid container spacing={2} mt={2}>
         {currentBlogs?.map((blog) => (
-          <Grid item xs={12} md={6} lg={4} xl={3} key={blog._id}
+          <Grid item xs={12} md={6} lg={4} key={blog._id}
           sx ={{
           minHeight: "350px",
           transition: "transform 0.2s ease-in-out",
