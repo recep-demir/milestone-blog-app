14c6b39d040353b2bd75d828ce005df0423e3471
recep-demir <demir.rp@gmail.com>
2025-02-24 11:32:52 +0100

    edit profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,10 +1,21 @@
-
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
+import { Container, Typography, Avatar, Box } from "@mui/material";
 
 const Profile = () => {
-  return (
-    <div>Profile</div>
-  )
-}
+  const { currentUser } = useSelector((state) => state.auth);
 
-export default Profile
+  return (
+    <Container sx={{ display: "flex", flexDirection: "column", alignItems: "center", mt: 5 }}>
+      <Avatar sx={{ width: 200, height: 200 }} />
+      <Typography variant="h5" sx={{ mt: 2 }}>
+        User Name: {currentUser?.username || "Unknown"}
+      </Typography>
+      <Typography variant="body1" sx={{ mt: 1 }}>
+        Email: {currentUser?.email || "Not available"}
+      </Typography>
+    </Container>
+  );
+};
+
+export default Profile;
